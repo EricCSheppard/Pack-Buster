d49da76920ccb82f813e544324af048aaae79700
EricCSheppard <EricCSheppard@gmail.com>
2023-01-24 11:14:16 -0500

    Adds ability to select a set to open a pack from

--- controllers/mtgapi.js
+++ controllers/mtgapi.js
@@ -7,16 +7,17 @@ require('dotenv').config()
 // CREATE ROUTER -------------------------------
 const router = express.Router()
 
-router.get('/newpack', async (req, res) => {
+// Makes API call for a booster pack using the set option seleted from the main page.
+router.post('/newpack', async (req, res) => {
     const mtgSet = req.body.set
+    // console.log('this is req.body', req.body)
     // Pulls 1 card
     // const mtgCard = await axios(`${process.env.API_URL}/cards/5f8287b1-5bb6-5f4c-ad17-316a40d5bb0c`)
-
     // Pulls booster of random cards
     const mtgCard = await axios(`${process.env.API_URL}/sets/${mtgSet}/booster`)
     const mtgCardPack = mtgCard.data.cards
     res.render('mtgapi/newpack.liquid', { mtgCardPack })
-    console.log(mtgCardPack)
+    // console.log(mtgCardPack)
 })
 
 module.exports = router
